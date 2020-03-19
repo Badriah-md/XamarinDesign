b941556b26c88881c4cc4cb16aebaf559ac7be29
bdoor <bdoor92@gmail.com>
2020-03-19 19:47:07 +0300

    first

---
+++ newProject/GreetPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿
+using Xamarin.Forms;
+
+namespace newProject
+{
+    public partial class GreetPage : ContentPage
+    {
+       
+        public GreetPage()
+        {
+            InitializeComponent();
+
+            slider.Value = 0.5;
+
+            Padding = Device.OnPlatform(                  iOS: new Thickness(0, 20, 0, 0),                 Android: new Thickness(0),                 WinPhone: new Thickness(0));
+
+        }
+
+       
+
+
+    }
+}
---
+++ newProject/interfaces.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace newProject
+{
+    public partial class interfaces : ContentPage
+    {
+        public interfaces()
+        {
+            InitializeComponent();
+        }
+    }
+}
